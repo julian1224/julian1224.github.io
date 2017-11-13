06fc847a88c5b4c7a1f7fd4ff5a61a9b7cfae818
Dio Brando <7iscool77@gmail.com>
2017-11-13 14:35:28 +0100

    added picture
    improved UI, corrected some margin, better functionality.

--- JScript/onclick.js
+++ JScript/onclick.js
@@ -23,25 +23,31 @@ function loadDoc() {
   switch(indexstr){
     case "home":
     document.getElementById('page').innerHTML = "home";
+    document.getElementById('redirect__button').innerHTML = "Let's see more about me.";
     xhttp.open("GET", "Include/txt_source_home.txt", true);
     xhttp.send();
     break;
     case "procsim":
     document.getElementById('page').innerHTML = "proc sim";
+    document.getElementById('redirect__button').innerHTML = "Let's see the proc sim";
     xhttp.open("GET", "Include/txt_source_procsim.txt", true);
     xhttp.send();
     break;
     case "trumped":
     document.getElementById('page').innerHTML = "the wall";
+    document.getElementById('redirect__button').innerHTML = "Let's see the Wall";
     xhttp.open("GET", "Include/txt_source_trumped.txt", true);
     xhttp.send();
     break;
     case "pendo":
     document.getElementById('page').innerHTML = "My Band";
+    document.getElementById('redirect__button').innerHTML = "Let's see myBand";
+
     xhttp.open("GET", "Include/txt_source_pendo.txt", true);
     xhttp.send();
     break;
     default:
+    document.getElementById('redirect__button').innerHTML = "Let's see... Nothing?";
     console.log("Error. Variable unknown.");
     break;
   }
@@ -70,7 +76,7 @@ function reDirect(){
     location.href="https://23669.hosts.ma-cloud.nl/p1.1/proj/Wall/wall.php";
     break;
     case "pendo":
-  // location.href="myband";
+    location.href="http://23669.hosts.ma-cloud.nl/p1.1/proj/MyBand/index.php?page=home";
     break;
   }
 
