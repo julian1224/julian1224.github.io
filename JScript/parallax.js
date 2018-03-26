132025a60e02babe6628ab283cdfc1132d655d94
Dio Brando <7iscool77@gmail.com>
2018-03-27 01:07:38 +0200

    now the 'main' site looks better

---
+++ JScript/parallax.js
@@ -0,0 +1,15 @@
+
+window.onscroll = paraFunk;
+
+function paraFunk(){
+  var backgroundImage = document.querySelector(".largeimage__parallax");
+
+  var scrollText = document.querySelector(".floating__text");
+
+var yOffset = window.pageYOffset;
+// document.getElementById("tellme").innerHTML = yOffset + "px";
+backgroundImage.style.backgroundPositionY = "-" + yOffset/3 + "px";
+
+  scrollText.style.right =  yOffset * 2 - 200 + "px";
+
+}
