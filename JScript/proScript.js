2a64a22c99b0698e1f78960dc4b29a98e92b046b
Dio Brando <7iscool77@gmail.com>
2018-03-28 19:13:35 +0200

    fixed 'read' buttons

--- JScript/proScript.js
+++ JScript/proScript.js
@@ -1,7 +1,15 @@
-
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+   document.getElementById("jojoAction").innerHTML = this.responseText;
+  }
+};
 
 var newInfobox = document.getElementById("prompt");
 newInfobox.style.display = "none";
+var clickaway = document.getElementById("clickaway");
+clickaway.style.display = "none";
+clickaway.addEventListener("click", closePrompt);
 
 for (i = 0; i<6; i++) {
 var view = "view" + i;
@@ -39,20 +47,54 @@ document.getElementById(read).addEventListener("click", readBit);
       //parallax
       window.open('http://23669.hosts.ma-cloud.nl/TheFlashbulb/FRO/parallax/', '_blank');
     };
+
     if (this.id == "view5"){
       //oldportfolio
-      window.open('https://23669.hosts.ma-cloud.nl/TheFlashbulb/procsim/', '_blank');
-    } else {
-      console.log("idk");
+      window.open('index1.html', '_blank');
     };
   }
+  //
   function readBit(){
     console.log("clicked read");
-    readIndex = this.id;
-    if (readIndex = 0){};
-    if (readIndex = 1){};
-    if (readIndex = 2){};
-    if (readIndex = 3){};
-    if (readIndex = 4){};
-    if (readIndex = 5){};
+    if (this.id == "read0"){
+      xhttp.open("GET", "Include/txt_source_procsim.txt", true);
+      xhttp.send();
+      newInfobox.style.display = "initial";
+      clickaway.style.display = "initial";
+    };
+    if (this.id == "read1"){
+      xhttp.open("GET", "Include/txt_source_trumped.txt", true);
+      xhttp.send();
+      newInfobox.style.display = "initial";
+      clickaway.style.display = "initial";
+    };
+    if (this.id == "read2"){
+      xhttp.open("GET", "Include/txt_source_pendo.txt", true);
+      xhttp.send();
+      newInfobox.style.display = "initial";
+      clickaway.style.display = "initial";
+    };
+    if (this.id == "read3"){
+      xhttp.open("GET", "Include/txt_source_MUSE.txt", true);
+      xhttp.send();
+      newInfobox.style.display = "initial";
+      clickaway.style.display = "initial";
+    };
+    if (this.id == "read4"){
+      xhttp.open("GET", "Include/txt_source_slider.txt", true);
+      xhttp.send();
+      newInfobox.style.display = "initial";
+      clickaway.style.display = "initial";
+    };
+    if (this.id == "read5"){
+      xhttp.open("GET", "Include/txt_source_myportfolio.txt", true);
+      xhttp.send();
+      newInfobox.style.display = "initial";
+      clickaway.style.display = "initial";
+    };
   }
+  //
+    function closePrompt(){
+      newInfobox.style.display = "none";
+      clickaway.style.display = "none";
+    }
